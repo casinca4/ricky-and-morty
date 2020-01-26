70dd43e2a4c0450bed304da78e72447eeea50ef6
casinca4 <s.antonelli@web.de>
2020-01-26 21:43:06 +0100

    Add navigation woof woof

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import Episode from './Episode';
+import '../scss/App.scss';
+
+const App = () => {
+  const [episodes, setEpisodes] = useState({ info: {}, results: [] });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const url = `https://rickandmortyapi.com/api/episode`;
+    const data = await (await fetch(url)).json();
+    setEpisodes(data);
+  };
+
+  const episodesList = episodes.results.map(el => {
+    return <Episode data={el} key={el.id}></Episode>;
+  });
+
+  return <div className="app">{episodesList}</div>;
+};
+
+export default App;
---
+++ src/components/Character.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import grumpy from '../statics/grumpy.jpeg';
+
+const Character = props => {
+  const { name, image } = props.data;
+
+  return (
+    <div className="character">
+      {image ? <img src={image} alt={name} /> : <img src={grumpy}></img>}
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default Character;
